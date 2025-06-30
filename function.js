39199f3ebe034f4c15e9d7622a1f5cca1a7122b0
Tshering <tsheringchozom55@gmail.com>
2025-06-30 13:05:29 +0600

    Adds replace function

--- function.js
+++ function.js
@@ -3,8 +3,29 @@
 // }
 // console.log(reverseString("Tshering"));
 
-function replace(str,charToReplace,strReplacement){}
+function replace(str,charToReplace,strReplacement){
+   let result="";
+    for(let i=0; i<str.length;i+=1){
+        if(str[i] === charToReplace){
+            result += strReplacement; 
+        }else{
+            result += str[i]
+        }
+    }
+return result;
+}
+console.log(replace("hello","o","0987"));
 
-function strLengthEven(str){}
 
-function findRabbit(arr){}
+
+
+
+
+
+
+
+
+
+// function strLengthEven(str){}
+
+// function findRabbit(arr){}
