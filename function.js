42ce6ce0c8499e8b9cab5fca971da1f0f9686ea4
Tshering <tsheringchozom55@gmail.com>
2025-06-30 12:25:38 +0600

    Adds function skeleton

--- function.js
+++ function.js
@@ -1,4 +1,10 @@
-function reverseString(str){
-    return str.split("").reverse().join("");
-}
-console.log(reverseString("Tshering"));
+// function reverseString(str){
+//     return str.split("").reverse().join("");
+// }
+// console.log(reverseString("Tshering"));
+
+function replace(str,charToReplace,strReplacement){}
+
+function strLengthEven(str){}
+
+function findRabbit(arr){}
